ecdd214ad1d89c6cdf5ad60a7404249ba0e5d063
KiDo <konvict_massari@yahoo.com>
2014-11-05 18:24:33 +0300

    Add Correct Command
    Lucy will correct the last sentence.

--- brain/plugins/turn.py
+++ brain/plugins/turn.py
@@ -1,12 +1,10 @@
 #===islucyplugin===
 # -*- coding: utf-8 -*-
 
-#  Lucy's Plugin
-#  turn_plugin.py
+#  Lucy's plugin
 
 #  Initial Copyright © 2008 dimichxp <dimichxp@gmail.com>
 #  Idea © 2008 Als <Als@exploit.in>
-#  Modifications Copyright © 2009 wd/lotusfeet <dao/yoga>
 
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -30,13 +28,11 @@ def handler_turn_last(type, source, parameters):
 		reply(type,source,reduce(lambda x,y:global_en2ru_table.get(x,x)+global_en2ru_table.get(y,y),parameters))
 	else:
 		if turn_msgs[groupchat][jid] is None:
-			reply(type,source,u' And you still did not say anything!')
+			reply(type,source,u'you said nothing')
 			return
-
-		if turn_msgs[groupchat][jid] == u'%sturn' % ():
-			reply(type,source,u'А вот фиг!')
-			return
-		
+		if turn_msgs[groupchat][jid] == u'turn':
+			reply(type,source,u'not allowed')
+			return		
 		tmsg=turn_msgs[groupchat][jid]
 		reply(type,source,reduce(lambda x,y:global_en2ru_table.get(x,x)+global_en2ru_table.get(y,y),tmsg))
 
@@ -56,7 +52,29 @@ def handler_turn_join(groupchat, nick, aff, role):
 	if not jid in turn_msgs[groupchat].keys() and jid != JID:
 		turn_msgs[groupchat][jid]=None
 
+def handler_correct_message(type, source, parameters):
+        groupchat = source[1]
+        nick = source[2]
+        jid = get_true_jid(groupchat+'/'+nick)
+        wword, seperator, rword = parameters.partition(' ')
+        message = ''
+        words = ''
+        wmessage = turn_msgs[groupchat][jid].strip().split()
+        if not wword in wmessage:
+                reply(type, source, u'No such word')
+                return
+        for i in range(0, len(wmessage)):
+                       if wword == wmessage[i]:
+                               message += rword
+                               message += ' '
+                       else:
+                               message += wmessage[i]
+                               message += ' '
+        msg(groupchat, nick+u' meant to say:\n'+message)
+        return
+        
 register_message_handler(handler_turn_save_msg)
 register_join_handler(handler_turn_join)
+register_command_handler(handler_turn_last, 'turn', ['turn','en','all'], 100, 'Commutes the lay-out of keyboard for the latest report from the user of causing command.', 'turn', ['turn'])
+register_command_handler(handler_correct_message, 'correct', ['turn','en','all'], 0, 'Type correct followed by the wrong and the right words in order to correct the last sentence.','correct wrong-word right-word',['hello gays'],['correct gays guys'])
 
-register_command_handler(handler_turn_last, 'turn', ['muc','all','*'], 10, 'Switch layout for the last message for the user of causing command.', 'turn', ['turn'])
